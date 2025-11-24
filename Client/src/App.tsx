fa5486a657b296649159179b79d4e2b933791794
Michel-Lalaina <michelramanantenasoa@gmail.com>
2025-11-24 13:39:23 +0300

    update

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,16 +1,20 @@
-import ArchitectureTree from "./components/ArchitectureTree";
+// import ArchitectureTree from "./components/ArchitectureTree";
 import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
 
   return (
 <>
-    <div className="p-6"> ICI ny point d ' entrer App <h1 className="text-green-700"> Groupe Michel et Carole </h1> 
-    <ArchitectureTree/>
+    <div className="p-6 ">
+      <h1 className="text-green-700"> Groupe Michel et Carole </h1> 
+    {/* <ArchitectureTree/> */}
+    <div className="flex">
+    <LoginPage/>
+    <ChatPage/>
     </div>
-    <div className="p-4">
-      <ChatPage/>
     </div>
+
 </>
   );
 }
